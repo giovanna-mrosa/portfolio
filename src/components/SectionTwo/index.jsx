ed3602f5a9d086fd1da7eb9acf78e188d52fa1e0
Giovanna Rosa <gmarcaro@gmail.com>
2022-04-17 15:29:05 -0300

    :sparkles: feat: Modal

--- src/components/SectionTwo/index.jsx
+++ src/components/SectionTwo/index.jsx
@@ -10,10 +10,46 @@ import screenSeven from '../../assets/tela7.svg'
 import screenEight from '../../assets/tela8.svg'
 import screenNine from '../../assets/tela9.svg'
 import bgWeb from '../../assets/bubble-bg-web.png'
+import closeIcon from '../../assets/close.svg'
+import devfinderImg from '../../assets/devfinder.svg'
 
 import './styles.scss'
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    zIndex: '4',
+    height: '450px',
+    width: '940px',
+    borderRadius: '18px',
+    position: 'relative',
+    border: 'none',
+    boxShadow: '0px 4px 4px 0px #00000040',
+    display: 'flex',
+    alignItems: 'flex-end',
+    padding: '40px',
+    gap: '60px',
+    background: '#EBE6E6'
+  },
+}
 
 export function SectionTwo() {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div className="container-two">
       <section className="about-me-section"  id="about-me">
@@ -35,48 +71,79 @@ export function SectionTwo() {
         <div className="projects">
           <div className="row">
             <div className="project">
-              <img src={screenOne} alt="" />
+              <img src={screenOne} alt="" className="mockup" onClick={openModal} />
               <p>devfinder</p>
             </div>
             <div className="project">
-              <img src={screenTwo} alt="" />
+              <img src={screenTwo} alt="" className="mockup" />
               <p>shortly</p>
             </div>
             <div className="project">
-              <img src={screenThree} alt="" />
+              <img src={screenThree} alt="" className="mockup" />
               <p>advice-genarator</p>
             </div>
           </div>
           <div className="row">
             <div className="project">
-              <img src={screenFour} alt="" />
+              <img src={screenFour} alt="" className="mockup" />
               <p>manage</p>
             </div>
             <div className="project">
-              <img src={screenFive} alt="" />
+              <img src={screenFive} alt="" className="mockup" />
               <p>blogr</p>
             </div>
             <div className="project">
-              <img src={screenSix} alt="" />
+              <img src={screenSix} alt="" className="mockup" />
               <p>huddle</p>
             </div>
           </div>
           <div className="row">
             <div className="project">
-              <img src={screenSeven} alt="" />
+              <img src={screenSeven} alt="" className="mockup" />
               <p>easy-class</p>
             </div>
             <div className="project">
-              <img src={screenEight} alt="" />
+              <img src={screenEight} alt="" className="mockup" />
               <p>splitter</p>
             </div>
             <div className="project">
-              <img src={screenNine} alt="" />
+              <img src={screenNine} alt="" className="mockup" />
               <p>areaflix</p>
             </div>
           </div>
         </div>
       </section>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <img src={devfinderImg} alt="" className="project1"/>
+        <img src={closeIcon} onClick={closeModal} alt="Close icon" className="close-icon" />
+        <div className="content-modal">
+          <h2 className="title-modal">devfinder</h2>          
+          <p className="description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis est aspernatur autem provident, ex cum modi. Alias amet minima voluptas quos beatae, facere nisi doloribus in vel ex? Exercitationem, ipsa?</p>
+          <div className="buttons-modal-box">
+            <button className="button-modal">
+              <a 
+                href="https://github.com/giovanna-mrosa/frontendMentor-devfinder"
+                target='_blank' rel="noreferrer"
+              >
+                Repo
+              </a>
+            </button>
+            <button className="button-modal">
+              <a 
+                href="https://frontend-mentor-devfinder.vercel.app/"
+                target='_blank' rel="noreferrer"
+              >
+                Demo
+              </a>
+            </button>
+          </div>
+        </div>
+      </Modal>
     </div>
   )
 }
